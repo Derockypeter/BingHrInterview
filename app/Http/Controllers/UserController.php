3e25cabfd90386ba4d4bf0b246da54b75cd24e87
LorenzoChukwuebuka <Lawrenceobi2@gmail.com>
2022-04-28 14:37:00 +0100

    migration files added with models and controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,18 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Validator;
 
 class UserController extends Controller
 {
     public function index()
-    {}
+    {
+      return response(["message"=>"hello"]);
+    }
 
     public function create(Request $request)
-    {}
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'username' => [],
+            'email' => 'required|email|unique:users',
+            'mobile_number' => 'required',
+            'role' => 'required',
+            'password' => 'required|min:5',
+
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
+        //creates user
+        $userCreate = User::create(array_merge($validator->validated(), [
+            'password' => Hash::make($request->password),
+        ]));
+
+        if ($userCreate) {
+            $userCreate->id;
+        }
+    }
 
     public function get_one($id)
-    {}
+    {
+
+    }
 
     public function update($id)
     {}
