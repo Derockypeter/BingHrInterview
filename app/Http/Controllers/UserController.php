40c0cb2f7871328cd73e36f87587c854f318601d
LorenzoChukwuebuka <Lawrenceobi2@gmail.com>
2022-04-28 12:03:27 +0100

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {}
+
+    public function create(Request $request)
+    {}
+
+    public function get_one($id)
+    {}
+
+    public function update($id)
+    {}
+
+    public function delete($id)
+    {}
+}
